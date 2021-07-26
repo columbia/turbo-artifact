abdd3a2bf4924a5255b88069a978b49bc5cda9c4
Calliope Kostopoulou <kelkost@yahoo.gr>
2021-07-26 19:29:35 +0300

    multi block ILP formulation

--- privacypacking/curves.py
+++ privacypacking/curves.py
@@ -12,6 +12,14 @@ class GaussianBudget(Budget):
         orders = {alpha: alpha / (2 * (sigma ** 2)) for alpha in alpha_list}
         super().__init__(orders)
 
+class MultiblockGaussianBudget():
+    def __init__(self, num_blocks, sigma: float, alpha_list: List[float] = ALPHAS) -> None:
+
+        self.block_budgets = []
+        for _ in range(num_blocks):
+            orders = {alpha: alpha / (2 * (sigma ** 2)) for alpha in alpha_list}
+            self.block_budgets += [Budget(orders)]
+            # super().__init__(orders)
 
 class SubsampledGaussianBudget(Budget):
     pass
---
+++ privacypacking/offline_multi_block_ILP.py
@@ -0,0 +1,72 @@
+"""
+Comparing DPF vs Packing
+Offline, multi block setting
+"""
+
+import random
+
+import gurobipy as gp
+import numpy as np
+from gurobipy import GRB
+
+from privacypacking.budget import Budget, ALPHAS
+from privacypacking.curves import GaussianBudget, MultiblockGaussianBudget
+from privacypacking.plot import save_fig, stack_jobs_under_block_curve
+
+
+def pack_many_blocks(job_list, blocks):
+
+    """
+    Returns a list of booleans corresponding to the jobs that are allocated
+    """
+    m = gp.Model("pack")
+    n = len(job_list)
+    d = len(ALPHAS)
+
+    demands_upper_bound = {}
+    for k, block in enumerate(blocks):
+        for alpha in block.alphas:
+            demands_upper_bound[(k,alpha)] = sum([job.block_budgets[k].orders[alpha] for job in job_list])
+
+    # Variables
+    x = m.addMVar((1,n), vtype=GRB.BINARY, name="x")
+    a = m.addMVar((len(blocks),d), vtype=GRB.BINARY, name="a")
+    print(x, a)
+
+    # Constraints
+    for k, _ in enumerate(blocks):
+        print(k)
+        m.addConstr(a[k].sum() >= 1)
+
+    for k, block in enumerate(blocks):
+        for i, alpha in enumerate(block.alphas):
+            print(demands_upper_bound[(k,alpha)])
+            print((1-a[k][i])*demands_upper_bound[(k,alpha)])
+            demands = [job.block_budgets[k].orders[alpha] for j, job in enumerate(job_list)]            # demands = demands.reshape([1,n])
+            m.addConstr(sum([x[0,z]*demands[z] for z in range(n)]) - (1-a[k,i])*demands_upper_bound[(k,alpha)] <= block.orders[alpha])
+    print(m)
+
+    # Objective function
+    m.setObjective(x.sum(), GRB.MAXIMIZE)
+    m.optimize()
+
+    return [bool((abs(x[0,i].x - 1) < 1e-4)) for i in range(n)]
+
+
+def main():
+    num_blocks = 2
+    blocks = []
+    for _ in range(num_blocks):
+        blocks += [Budget.from_epsilon_delta(epsilon=10, delta=0.001)]
+
+    jobs = [MultiblockGaussianBudget(num_blocks, sigma=s) for s in np.linspace(0.1, 1, 10)]
+
+    random.shuffle(jobs)
+
+    allocation = pack_many_blocks(jobs, blocks)
+    print(allocation)
+    # stack_jobs_under_block_curve(jobs, blocks, allocation)
+
+
+if __name__ == "__main__":
+    main()
