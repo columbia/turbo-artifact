ffd30501af8277bf9ccce4ad730807c6eaae9a68
Pierre Tholoniat <pierre@tholoniat.com>
2021-08-04 18:13:06 -0400

    Added  field, removed num_blocks and non-essential fields, updated UniformTask

--- privacypacking/budget/task.py
+++ privacypacking/budget/task.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Dict, Iterable
 
 from privacypacking.budget.budget import Budget
 from privacypacking.budget.curves import (
@@ -10,70 +10,62 @@ from privacypacking.budget.curves import (
 from privacypacking.utils.utils import *
 
 
-# TODO: should we store a dict of blocks, to avoid `get_block_by_id`?
 class Task:
-    def __init__(self, id, num_blocks, block_ids, type):
+    def __init__(self, id: int, profit: float, budget_per_block: Dict[int, "Budget"]):
         self.id = id
+        self.profit = profit
+        self.budget_per_block = budget_per_block  # block_id -> Budget
 
-        # TODO: make this optional? Some tasks are a combination, no type
-        self.type = type
-        self.budget_per_block = {}  # block_id -> Budget
+    def get_budget(self, block_id: int) -> Budget:
+        """
+        Args:
+            block_id (int): a block id
 
-        # TODO: I don't think that we need this field in general.
-        # The process that generates the Task might need `num_blocks` to assign the blocks,
-        # but the Task itself doesn't need to know anything else
+        Returns:
+            Budget: the budget of the block if demanded by the task, else ZeroCurve
+        """
 
-        self.num_blocks = (
-            num_blocks  # current total number of blocks in the environment
-        )
-
-        self.block_ids = block_ids  # block ids requested by task
-        # Initialize all block demands to zero
-        # TODO: I think that zero budget should be implicit, because we might want to
-        # add more blocks over time. Also, it can be slow to keep track of all the blocks
-        # even if the task only cares about 1 or 2 blocks.
-        for i in range(num_blocks):
-            self.budget_per_block[i] = ZeroCurve().budget
-
-    # TODO: this is a weird name/API
-    def get_budget_or_zero(self, block_id: int) -> Budget:
         if block_id in self.budget_per_block:
             return self.budget_per_block[block_id]
         else:
-            return ZeroCurve().budget
+            return ZeroCurve()
 
 
 class UniformTask(Task):
-    # For the general case
-    def __init__(self, id: int, block_ids: Iterable[int], budget: Budget):
-        self.id = id
-        self.budget_per_block = {}
+    def __init__(
+        self, id: int, profit: float, block_ids: Iterable[int], budget: Budget
+    ):
+        """
+        A Task that requires (the same) `budget` for all blocks in `block_ids`
+        """
+        budget_per_block = {}
         for block_id in block_ids:
-            # Keep an eye on the memory usage here (maybe we're fine without copy)
-            self.budget_per_block[block_id] = budget.copy()
+            budget_per_block[block_id] = budget
+        super().__init__(id, profit, budget_per_block)
 
 
-def create_laplace_task(task_id, num_blocks, block_ids, noise):
-    # Same curve for all blocks for now / same demands per block
-    task = Task(task_id, num_blocks, block_ids, LAPLACE)
-    for block_id in block_ids:
-        task.budget_per_block[block_id] = LaplaceCurve(laplace_noise=noise).budget
-    return task
+# Deprecated. Use UniformTask instead.
+# def create_laplace_task(task_id, num_blocks, block_ids, noise):
+#     # Same curve for all blocks for now / same demands per block
+#     task = Task(task_id, num_blocks, block_ids, LAPLACE)
+#     for block_id in block_ids:
+#         task.budget_per_block[block_id] = LaplaceCurve(laplace_noise=noise).budget
+#     return task
 
 
-def create_gaussian_task(task_id, num_blocks, block_ids, sigma):
-    # Same curve for all blocks for now / same demands per block
-    task = Task(task_id, num_blocks, block_ids, GAUSSIAN)
-    for block_id in block_ids:
-        task.budget_per_block[block_id] = GaussianCurve(sigma=sigma).budget
-    return task
+# def create_gaussian_task(task_id, num_blocks, block_ids, sigma):
+#     # Same curve for all blocks for now / same demands per block
+#     task = Task(task_id, num_blocks, block_ids, GAUSSIAN)
+#     for block_id in block_ids:
+#         task.budget_per_block[block_id] = GaussianCurve(sigma=sigma).budget
+#     return task
 
 
-def create_subsamplegaussian_task(task_id, num_blocks, block_ids, ds, bs, epochs, s):
-    # Same curve for all blocks for now / same demands per block
-    task = Task(task_id, num_blocks, block_ids, SUBSAMPLEGAUSSIAN)
-    for block_id in block_ids:
-        task.budget_per_block[
-            block_id
-        ] = SubsampledGaussianCurve.from_training_parameters(ds, bs, epochs, s).budget
-    return task
+# def create_subsamplegaussian_task(task_id, num_blocks, block_ids, ds, bs, epochs, s):
+#     # Same curve for all blocks for now / same demands per block
+#     task = Task(task_id, num_blocks, block_ids, SUBSAMPLEGAUSSIAN)
+#     for block_id in block_ids:
+#         task.budget_per_block[
+#             block_id
+#         ] = SubsampledGaussianCurve.from_training_parameters(ds, bs, epochs, s).budget
+#     return task
--- privacypacking/offline/schedulers/scheduler.py
+++ privacypacking/offline/schedulers/scheduler.py
@@ -1,7 +1,9 @@
 from typing import List
 
-
 # QUESTION: should `schedule` modify the blocks inplace? (reduce the budget available)
+# TODO: should we store a dict of blocks, to avoid `get_block_by_id`?
+
+
 # Or should it just return an allocation, that we execute in a second step?
 class Scheduler:
     def __init__(self, tasks, blocks):
