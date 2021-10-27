2ef5e26d32ab2afb1e413336ea3039ba0c9886bb
Calliope Kostopoulou <kelkost@yahoo.gr>
2021-10-27 17:03:57 -0400

    experiments

---
+++ experiments/ray/offline_mice_and_elephants/metrics_comparison.py
@@ -0,0 +1,70 @@
+import os
+
+import argparse
+from loguru import logger
+from ray import tune
+import numpy as np
+from privacypacking.simulator.simulator import Simulator
+from privacypacking.utils.utils import *
+from privacypacking.schedulers.utils import (
+    BASIC_SCHEDULER,
+    SIMPLEX,
+    DOMINANT_SHARES,
+    FLAT_RELEVANCE,
+    OVERFLOW_RELEVANCE
+)
+from privacypacking.config import Config
+
+
+def run_and_report(config: dict) -> None:
+    os.environ["LOGURU_LEVEL"] = "INFO"
+    sim = Simulator(Config(config))
+    metrics = sim.run()
+    logger.info(metrics)
+    tune.report(**metrics)
+
+
+def grid():
+    scheduler_methods = [BASIC_SCHEDULER]
+    scheduler_metrics = [DOMINANT_SHARES, FLAT_RELEVANCE, OVERFLOW_RELEVANCE]
+    block_selection_policies = ["RandomBlocks"]
+    data_task_frequencies_path = [
+        "mice_0.yaml",
+        "mice_20.yaml",
+        "mice_40.yaml",
+        "mice_60.yaml",
+        "mice_80.yaml",
+        "mice_100.yaml",
+    ]
+    config[SCHEDULER_SPEC][METHOD] = tune.grid_search(scheduler_methods)
+    config[SCHEDULER_SPEC][METRIC] = tune.grid_search(scheduler_metrics)
+    # config[TASKS_SPEC][CURVE_DISTRIBUTIONS][CUSTOM][INITIAL_NUM] = tune.grid_search(
+    #     np.arange(1, 100, step=1, dtype=int).tolist()
+    # )
+    config[TASKS_SPEC][CURVE_DISTRIBUTIONS][CUSTOM][
+        READ_BLOCK_SELECTION_POLICY_FROM_CONFIG
+    ][BLOCK_SELECTING_POLICY] = tune.grid_search(block_selection_policies)
+    config[TASKS_SPEC][CURVE_DISTRIBUTIONS][CUSTOM][
+        DATA_TASK_FREQUENCIES_PATH
+    ] = tune.grid_search(data_task_frequencies_path)
+
+    tune.run(
+        run_and_report,
+        config=config,
+        resources_per_trial={"cpu": 1},
+        local_dir=RAY_LOGS,
+        resume=False,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", dest="config_file")
+    args = parser.parse_args()
+    with open(DEFAULT_CONFIG_FILE, "r") as f:
+        config = yaml.safe_load(f)
+    with open(args.config_file, "r") as user_config:
+        user_config = yaml.safe_load(user_config)
+    update_dict(user_config, config)
+
+    grid()
