e3503a7ec90136fa309beee873341f2279ce6db3
Pierre Tholoniat <pierre@tholoniat.com>
2022-12-20 17:06:41 -0500

    fix marginal queries issue and arbitrary length queries

--- data/covid19/covid19_queries/queries.py
+++ data/covid19/covid19_queries/queries.py
@@ -1,6 +1,7 @@
 import json
 import math
 from itertools import chain, combinations, product
+from pathlib import Path
 from typing import Optional
 
 import typer
@@ -50,6 +51,7 @@ def create_all_queries(queries_path, attributes_domain_sizes):
 
 def create_specific_queries(queries_path, type="positive_cases"):
     queries = []
+    attributes_domain_sizes = [2, 2, 4, 8]
 
     if type == "positive_cases":
         queries = [[[1], [0, 1], [0, 1, 2, 3], [0, 1, 2, 3, 4, 5, 6, 7]]]
@@ -61,7 +63,6 @@ def create_specific_queries(queries_path, type="positive_cases"):
             query_tensors.append(query)
     elif type == "2way_marginal_mix":
         query_tensors = []
-        attribute_sizes = [2, 2, 4, 8]
         dicts = [
             {0: 1, 1: 0},
             {0: 1, 1: 1},
@@ -74,21 +75,60 @@ def create_specific_queries(queries_path, type="positive_cases"):
             query_tensors.append(k_way_marginal_query_list(d, attribute_sizes))
     elif type == "all_2way_marginals":
         query_tensors = []
-        attribute_sizes = [2, 2, 4, 8]
         dicts = []
 
         # Conjunctions of 0 with something else, then 1 with something else than 0, etc.
         for i in range(4):
-            for a in range(attribute_sizes[i]):
-                for j in range(i, 4):
-                    for b in range(attribute_sizes[i]):
+            for a in range(attributes_domain_sizes[i]):
+                for j in range(i + 1, 4):
+                    for b in range(attributes_domain_sizes[j]):
+                        print(f"{i}: {a}, {j}: {b}")
                         dicts.append({i: a, j: b})
 
-        # Only 124 2-way marginals! Pretty small, let's see if that's enough for PMW to shine
+        # Only 84 2-way marginals! Pretty small, let's see if that's enough for PMW to shine
         print(f"We have {len(dicts)} 2-way marginals. Computing the tensors...")
 
         for d in dicts:
-            query_tensors.append(k_way_marginal_query_list(d, attribute_sizes))
+            query_tensors.append(k_way_marginal_query_list(d, attributes_domain_sizes))
+
+    elif type == "all_3way_marginals":
+        query_tensors = []
+        dicts = []
+
+        # Conjunctions of 0 with something else, then 1 with something else than 0, etc.
+        for i in range(4):
+            for a in range(attributes_domain_sizes[i]):
+                for j in range(i + 1, 4):
+                    for b in range(attributes_domain_sizes[j]):
+                        for k in range(j + 1, 4):
+                            for c in range(attributes_domain_sizes[k]):
+                                print(f"{i}: {a}, {j}: {b}, {k}: {c}")
+                                dicts.append({i: a, j: b, k: c})
+
+        # 176 only
+        print(f"We have {len(dicts)} 3-way marginals. Computing the tensors...")
+
+        for d in dicts:
+            query_tensors.append(k_way_marginal_query_list(d, attributes_domain_sizes))
+
+    else:
+        num_queries = int(type)
+        queries = []
+        attr_values = []
+        for domain_size in attributes_domain_sizes:
+            attr_values += [list(powerset(range(domain_size)))[1:]]  # Exclude empty set
+        queries = list(product(*attr_values))
+
+        query_tensors = []
+        for query in queries:
+            query = [list(tup) for tup in query]
+            query = [list(tup) for tup in product(*query)]
+            query_tensors.append(query)
+
+    assert num_queries <= len(query_tensors)
+    print(f"Keeping {num_queries} queries from the total {len(query_tensors)} queries.")
+
+    query_tensors = query_tensors[0:num_queries]
 
     # Write queries to a json file
     queries = {}
@@ -96,9 +136,6 @@ def create_specific_queries(queries_path, type="positive_cases"):
         queries[i] = query
 
     json_object = json.dumps(queries, indent=4)
-
-    # NOTE: this representation only handles counting queries (bin value in {0,1})
-    # we might need to handle arbitrary linear queries later (dump sparse tensors directly)
 
     # Writing to queries.json
     with open(queries_path, "w") as outfile:
@@ -139,7 +176,6 @@ class QueryPool:
         return query
 
 
-@app.command()
 def main(
     queries_dir: str = REPO_ROOT.joinpath("data/covid19/covid19_queries"),
     blocks_metadata_path: str = REPO_ROOT.joinpath(
@@ -162,9 +198,9 @@ def main(
 
     # create_specific_queries(queries_path)
     # create_specific_queries(queries_path, type="2way_marginal_mix")
-    queries_path = queries_dir.joinpath(f"{workload}.queries.json")
+    queries_path = Path(queries_dir).joinpath(f"{workload}.queries.json")
     create_specific_queries(queries_path, type=workload)
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
